2c6d1e9d4dfb9ef47abb2f98f5b6baa1739966f4
vadrian1 <123913872+vadrian1@users.noreply.github.com>
2024-04-07 17:40:05 -0400

    ccp

--- js/js.js
+++ js/js.js
@@ -7,3 +7,21 @@ function menu() {
         menu.classList.add("hidden");
     }
 }
+
+var galleryButtons = document.getElementsByClassName("galleryButton")
+for(var i = 0; i < galleryButtons.length; i++){
+    galleryButtons[i].addEventListener("click", showGallery)
+    galleryButtons[i].nextElementSibling.classList.add("hidden");
+    galleryButtons[i].innerHTML = "Show images"
+}
+
+function showGallery(){
+    var gallery = this.nextElementSibling
+    if (gallery.classList.contains("hidden")) {
+        gallery.classList.remove("hidden");
+        this.innerHTML = "Hide images"
+    } else {
+        gallery.classList.add("hidden");
+        this.innerHTML = "Show images"
+    }
+}
