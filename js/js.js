5655a442876e4e4039a838ea867c2e6029a2baa4
vadrian1 <123913872+vadrian1@users.noreply.github.com>
2024-04-07 19:38:46 -0400

    fdsfss

--- js/js.js
+++ js/js.js
@@ -1,11 +1,7 @@
 document.getElementById("menu").classList.remove("hidden");
 function menu() {
     var menu = document.getElementById("menu");
-    if (menu.classList.contains("hidden")) {
-        menu.classList.remove("hidden");
-    } else {
-        menu.classList.add("hidden");
-    }
+    menu.classList.toggle("hidden")
 }
 
 var galleryButtons = document.getElementsByClassName("galleryButton")
@@ -17,11 +13,11 @@ for(var i = 0; i < galleryButtons.length; i++){
 
 function showGallery(){
     var gallery = this.nextElementSibling
-    if (gallery.classList.contains("hidden")) {
-        gallery.classList.remove("hidden");
+    gallery.classList.toggle("hidden")
+    if(this.innerHTML == "Show images"){
         this.innerHTML = "Hide images"
-    } else {
-        gallery.classList.add("hidden");
+    }
+    else{
         this.innerHTML = "Show images"
     }
 }
